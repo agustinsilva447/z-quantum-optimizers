6d1fc7d2be1d981e5e18bf4140a5386e14321916
agustinsilva447 <agustinsilva447@gmail.com>
2020-07-23 09:37:47 -0300

    cvar

--- src/python/zquantum/optimizers/cvar_optimizer.py
+++ src/python/zquantum/optimizers/cvar_optimizer.py
@@ -9,50 +9,72 @@ import numpy as np
 from zquantum.core.interfaces.optimizer import Optimizer
 from scipy.optimize import OptimizeResult
 
+# set classical optimizer
+maxiter = 100
+optimizer = COBYLA(maxiter=maxiter)
+# set variational ansatz
+var_form = RY(n, depth=1)
+# var_form = QAOAVarForm(H, 1)  # use this ansatz for CVaR-QAOA
+m = var_form.num_parameters
+# set backend
+backend = Aer.get_backend('statevector_simulator')
+# backend = Aer.get_backend('qasm_simulator')  # use this for QASM simulator
+
+def compute_cvar(probabilities, values, alpha):
+    """ 
+    Auxilliary method to computes CVaR for given probabilities, values, and confidence level.
+    
+    Attributes:
+    - probabilities: list/array of probabilities
+    - values: list/array of corresponding values
+    - alpha: confidence level
+    
+    Returns:
+    - CVaR
+    """
+    sorted_indices = np.argsort(values)
+    probs = np.array(probabilities)[sorted_indices]
+    vals = np.array(values)[sorted_indices]
+    cvar = 0
+    total_prob = 0
+    for i, (p, v) in enumerate(zip(probs, vals)):
+        done = False
+        if p >= alpha - total_prob:
+            p = alpha - total_prob
+            done = True
+        total_prob += p
+        cvar += p * v
+    cvar /= total_prob
+    return cvar
+
+def eval_bitstring(H, x):
+    """
+    Auxilliary method to evaluate the objective function for a given bitstring
+    
+    Attributes:
+    - H: cost Hamiltonian
+    - x: bitstring
+    
+    Returns:
+    - objective value
+    """
+
+    # invert bitstring for convenience and translate to +/-1
+    x = x[::-1]    
+    spins = np.array([(-1)**(b == '1') for b in x])
+    value = 0
+    
+    # loop over pauli terms and add contribution to objective
+    for p in H.paulis:
+        weight = np.real(p[0])
+        indices = np.where(p[1].z)
+        value += weight * np.prod(spins[indices])
+    return value
+
 class CVAROptimizer(Optimizer):
-
-    def __init__(self, options):
-        
-    def minimize(self, cost_function, initial_params):
-        
-        # Optimization Results Object
-        history = []
-        
-        maxiter = 100
-        optimizer = COBYLA(maxiter=maxiter)
-        var_form = QAOAVarForm(H, 1)  # use this ansatz for CVaR-QAOA
-        m = var_form.num_parameters
-        backend = Aer.get_backend('statevector_simulator')  
-
-        def compute_cvar(probabilities, values, alpha):
-            sorted_indices = np.argsort(values)
-            probs = np.array(probabilities)[sorted_indices]
-            vals = np.array(values)[sorted_indices]
-            cvar = 0
-            total_prob = 0
-            for i, (p, v) in enumerate(zip(probs, vals)):
-                done = False
-                if p >= alpha - total_prob:
-                    p = alpha - total_prob
-                    done = True
-                total_prob += p
-                cvar += p * v
-            cvar /= total_prob
-            return cvar
-                
-        def eval_bitstring(H, x):
-            # invert bitstring for convenience and translate to +/-1
-            x = x[::-1]    
-            spins = np.array([(-1)**(b == '1') for b in x])
-            value = 0
-            # loop over pauli terms and add contribution to objective
-            for p in H.paulis:
-                weight = np.real(p[0])
-                indices = np.where(p[1].z)
-                value += weight * np.prod(spins[indices])
-            return value
-
-class Objective:
+    """
+    Wrapper for objective function to track the history of evaluations
+    """
     
     def __init__(self, var_form, H, offset, alpha, backend, optimal=None):
         self.history = []
